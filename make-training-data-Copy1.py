98624afe76fdf1c6e7362ce40cb1fc511906300a
Dante Gates <dantegates@hotmail.com>
2017-10-16 22:02:57 -0400

    progress

--- make-training-data-Copy1.py
+++ make-training-data-Copy1.py
@@ -14,25 +14,23 @@ DESIRED_SAMPLE_RATE = 44100
 HOP_LENGTH = 2048
 N_CLIPS = 20
 OUTPUT_DIR = '/home/dante_gates/repos/music-rec/data/train'
-MAXSIZE = 30  # in megabytes
-SKIPFILE = 'success.txt'
+MAXSIZE = 40  # in megabytes
+SUCCESSFILE = 'success.txt'
 FAILFILE = 'failed.txt'
-FAILFILE2 = 'skip.txt'
+SKIPFILE = 'skip.txt'
 
-files = glob.glob('/home/dante_gates/music/Music/**/.wav', recursive=True) \
+FILES = glob.glob('/home/dante_gates/music/Music/**/.wav', recursive=True) \
       + glob.glob('/home/dante_gates/music/Music/**/*mp3', recursive=True)
 
 
 class UnreadableMP3Error(ValueError): pass
 class InvalidSampleRateError(ValueError): pass
-class MonoFileError(ValueError): pass
-class LargeFileError(ValueError): pass
 class InvalidShapeError(ValueError): pass
 
 def _mp3_hook(f):
     try:
         audio_segment = pydub.AudioSegment.from_mp3(f)
-    except Exception as err:
+    except (OSError, pydub.exceptions.CouldntDecodeError) as err:
         raise UnreadableMP3Error('could not read mp3') from err
     byte_stream = io.BytesIO()
     audio_segment.export(byte_stream, 'wav')
@@ -42,34 +40,27 @@ def _mp3_hook(f):
 def _read_wav(f):
     """Read wav file and return the sample rate and audio."""
     sr, audio = scipy.io.wavfile.read(f)
-    return _process_audio(sr, audio)
-
-def _process_audio(sr, audio):
-    if sr < DESIRED_SAMPLE_RATE:
-        raise InvalidSampleRateError('audio has a sample rate less than %s'
-                                % DESIRED_SAMPLE_RATE)
+    if len(audio.shape) == 2:
+        audio = (audio.sum(axis=1) / 2).astype("int16")
     return sr, audio
 
 # TODO: not doing anything with right channel now.
 def _make_features(sr, audio):
     n_samples = audio.shape[0]
-    try:
-        L, R = audio[:, 0], audio[:, 1]
-    except IndexError:
-        raise MonoFileError('not a stereo file')
     window = sr * WINDOW_LENGTH
     samples = random.sample(list(range(n_samples - window)), k=N_CLIPS)
     melspecs = []
     for clip_begin in samples:
-        clip = L[clip_begin:clip_begin+window]
+        clip = audio[clip_begin:clip_begin+window]
         melspec = librosa.feature.melspectrogram(clip, sr, hop_length=HOP_LENGTH)
         melspecs.append(np.reshape(melspec, -1))
     return melspecs
 
 _megabyte = 2**20
-def _getsize(f):
+def _valid_size(f):
     """Return size of `f` in megabytes."""
-    return os.path.getsize(f) / _megabyte
+    size = os.path.getsize(f) / _megabyte
+    return size <= MAXSIZE
 
 _expected_shape = (8320,)  # for sanity check
 def create_training_data(files):
@@ -85,11 +76,11 @@ def create_training_data(files):
                   % (success, skipped, os.path.basename(file))
             print(msg, end='', flush=True)
             _create_training_data(file)
-        except UnreadableMP3Error as err:
+        except (UnreadableMP3Error, InvalidSampleRateError) as err:
             skipped += 1
             failures.append((file, err))
-            with open(FAILFILE2, 'a') as f:
-                f.write('%s: %s\n' % (err, file))            
+            with open(SKIPFILE, 'a') as f:
+                f.write('%s\n' % file)
         except ValueError as err:
             skipped += 1
             failures.append((file, err))
@@ -98,14 +89,11 @@ def create_training_data(files):
         else:
             success += 1
             successes.append(file)
-            with open(SKIPFILE, 'a') as f:
+            with open(SUCCESSFILE, 'a') as f:
                 f.write('%s\n' % file)
 
 
 def _create_training_data(file):
-    filesize = _getsize(file)
-    if filesize > MAXSIZE:
-        raise LargeFileError('file too large, %s MB' % filesize)
     f = _mp3_hook(file) if file.endswith('.mp3') else file
     sr, audio = _read_wav(f)
     features = _make_features(sr, audio)
@@ -119,11 +107,20 @@ def _create_training_data(file):
             raise InvalidShapeError('Unexpected feature shape, %s' % feature.shape)
 
 
-if __name__ == '__main__':
+def _filter_input_files(files):
+    skip = set()
+    with open(SUCCESSFILE) as f:
+        skip |= {L for L in f.read().split('\n')}
     with open(SKIPFILE) as f:
-        skip1 = {L for L in f.read().split('\n')}
-    with open(FAILFILE2) as f:
-        skip2 = {L for L in f.read().split('\n')}
-    skip1.update(skip2)
-    files = [f for f in files if not f in skip1]
+        skip |= {L for L in f.read().split('\n')}
+    files = (f for f in files if not f in skip)
+    files = [f for f in files if _valid_size(f)]
+    return files
+
+
+if __name__ == '__main__':
+    print('found %s files total' % len(FILES))
+    files = _filter_input_files(FILES[1000:])
+    print('processing %s files' % len(files))
     create_training_data(files)
+
