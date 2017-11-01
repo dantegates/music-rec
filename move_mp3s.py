8f1f328759424bf2c826ae10ce22b5a1f0e8874c
Dante Gates <dantegates@hotmail.com>
2017-11-01 17:59:17 -0400

    exception handling

--- move_mp3s.py
+++ move_mp3s.py
@@ -4,26 +4,38 @@ import logging
 import readline
 import os
 import shutil
+import string
 
 import pydub
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level='INFO', filename='move.log', filemode='a')
+logging.basicConfig(level='INFO', filename='move.log', filemode='w')
+
+
+class CouldNotReadMetaData(ValueError):
+    def __init__(self, file):
+        super().__init__('could not read metadata on %s' % file)
 
 
 MetaData = collections.namedtuple('MetaData', 'artist,album,track')
 def get_meta_data(f):
     try:
         info = pydub.utils.mediainfo(f)['TAG']
+        artist, album, track = info['artist'], info['album'], info['title']
     except Exception as err:
-        raise ValueError('could not read metadata: %s' % f) from err
-    artist, album, track = info['artist'], info['album'], info['title']
+        raise CouldNotReadMetaData(f) from err
     return MetaData(artist, album, track)
 
 
 _path_format = '{artist}-{album}-{track}.mp3'
+def _clean_metadata(text):
+    for c in string.punctuation:
+        text = text.replace(c, '')
+    return text
+
+
 def rename(metadata):
-    return '-'.join(metadata) + '.mp3'
+    return '-'.join(_clean_metadata(d) for d in metadata) + '.mp3'
 
 
 def main(root_dir, output_dir):
@@ -37,9 +49,7 @@ def main(root_dir, output_dir):
             shutil.move(f, dst)
             logger.info('moved %s to %s' % (f, dst))
         except Exception as err:
-            logger.error('could not move %s: %s' % (f, err))
-        
-
+            logger.exception(err)
 
 
 if __name__ == '__main__':
